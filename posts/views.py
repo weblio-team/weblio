aba066f2d198f4fde7ce5ed5047b82be166d3cd0
Majo <monokumajo@fpuna.edu.py>
2024-08-30 21:15:28 -0400

    Aplicacion de permisos de Category
    01. Se agregan condicionales relacionadas a los permisos `add_category`, `change_category` y `delete_category` en:
    - `base.html` en la app members
    - `views.py` en la app posts
    - `category.html` en la app posts

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from .models import Category
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .forms import CategoryForm, CategoryEditForm
@@ -14,10 +15,11 @@ class CategoriesView(ListView):
     template_name = 'categories/categories.html'
     ordering = ['-id']
 
-class CategoryAddView(CreateView):
+class CategoryAddView(PermissionRequiredMixin, CreateView):
     model = Category
     form_class = CategoryForm
     template_name = 'categories/category_add.html'
+    permission_required = 'catalog.add_category'
 
 class CategoryDetailView(DetailView):
     model = Category
@@ -27,38 +29,31 @@ class CategoryDetailView(DetailView):
     def get_object(self):
         pk = self.kwargs.get("pk")
         name = self.kwargs.get("name")
-        return get_object_or_404(Category,
-            pk=pk,
-            name__iexact=name.replace('-', ' ')
-        )
+        return get_object_or_404(Category, pk=pk, name__iexact=name.replace('-', ' '))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['category_count'] = Category.objects.count()
         return context
 
-class CategoryEditView(UpdateView):
+class CategoryEditView(PermissionRequiredMixin, UpdateView):
     model = Category
     form_class = CategoryEditForm
     template_name = 'categories/category_edit.html'
+    permission_required = 'catalog.change_category'
     
     def get_object(self):
         pk = self.kwargs.get("pk")
         name = self.kwargs.get("name")
-        return get_object_or_404(Category,
-            pk=pk,
-            name__iexact=name.replace('-', ' ')
-        )
+        return get_object_or_404(Category, pk=pk, name__iexact=name.replace('-', ' '))
 
-class CategoryDeleteView(DeleteView):
+class CategoryDeleteView(PermissionRequiredMixin, DeleteView):
     model = Category
     template_name = 'categories/category_delete.html'
     success_url = reverse_lazy('categories')
+    permission_required = 'catalog.delete_category'
 
     def get_object(self):
         pk = self.kwargs.get("pk")
         name = self.kwargs.get("name")
-        return get_object_or_404(Category,
-            pk=pk,
-            name__iexact=name.replace('-', ' ')
-        )
+        return get_object_or_404(Category, pk=pk, name__iexact=name.replace('-', ' '))
