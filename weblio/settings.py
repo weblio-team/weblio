604297245d6734f008586a3d105fbfdce10b1020
kevin <kevinhttps@fpuna.edu.py>
2024-09-19 21:59:05 -0400

    Implementacion de compartir en redes sociales.

--- weblio/settings.py
+++ weblio/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import os
 import dj_database_url
 from google.oauth2 import service_account
-import urllib.parse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +30,7 @@ INSTALLED_APPS = [
     'ckeditor_uploader',
     'storages',
     'simple_history',
+    'django_social_share',
 ]
 
 MIDDLEWARE = [
